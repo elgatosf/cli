d582acdb7ea12fa971dc6967fe5772d0b25e0ae4
Richard Herman <geekyeggo@users.noreply.github.com>
2023-07-03 17:08:08 +0100

    refactor: simplified i18n for dev command

--- src/i18n/en.ts
+++ src/i18n/en.ts
@@ -2,14 +2,12 @@ import chalk from "chalk";
 
 const resources: Resources = {
 	commandError: (name) => `Failed to run command "${name}"`,
-	commands: {
-		dev: {
-			description: "Enable Stream Deck Plugin development.",
-			failed: "Failed to enable Stream Deck developer mode",
-			macOSErrorCode: (status) => `Encountered error code: ${status}`,
-			success: "Successfully enabled Stream Deck developer mode.",
-			unsupportedOS: "Unsupported operating system."
-		}
+	dev: {
+		description: "Enable Stream Deck Plugin development.",
+		failed: "Failed to enable Stream Deck developer mode",
+		macOSErrorCode: (status) => `Encountered error code: ${status}`,
+		success: "Successfully enabled Stream Deck developer mode.",
+		unsupportedOS: "Unsupported operating system."
 	},
 	link: {
 		aborted: "Linking aborted",
