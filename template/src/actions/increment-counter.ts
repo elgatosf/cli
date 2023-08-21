d01441dd1ad047412dccccff45bc07e1b1f7274b
Richard Herman <geekyeggo@users.noreply.github.com>
2023-08-21 14:47:05 +0100

    update template to latest lib version

--- template/src/actions/increment-counter.ts
+++ template/src/actions/increment-counter.ts
@@ -1,25 +1,25 @@
-import * as streamDeck from "@elgato/streamdeck";
+import { KeyDownEvent, SingletonAction, WillAppearEvent } from "@elgato/streamdeck";
 
 /**
  * An example action class that displays a count that increments by one each time the button is pressed.
  */
-export class IncrementCounter extends streamDeck.SingletonAction<CounterSettings> {
+export class IncrementCounter extends SingletonAction<CounterSettings> {
 	/**
-	 * The {@link streamDeck.client.onWillAppear} event is useful for setting the visual representation of an action when it become visible. This could be due to the Stream Deck first
+	 * The {@link SingletonAction.onWillAppear} event is useful for setting the visual representation of an action when it become visible. This could be due to the Stream Deck first
 	 * starting up, or the user navigating between pages / folders etc.. There is also an inverse of this event in the form of {@link streamDeck.client.onWillDisappear}. In this example,
 	 * we're setting the title to the "count" that is incremented in {@link IncrementCounter.onKeyDown}.
 	 */
-	onWillAppear(ev: streamDeck.WillAppearEvent<CounterSettings>): void | Promise<void> {
+	onWillAppear(ev: WillAppearEvent<CounterSettings>): void | Promise<void> {
 		return ev.action.setTitle(`${ev.payload.settings.count ?? 0}`);
 	}
 
 	/**
-	 * Listens for the {@link streamDeck.Client.onKeyDown} event which is emitted by Stream Deck when an action is pressed. Stream Deck provides various events for tracking interaction
+	 * Listens for the {@link SingletonAction.onKeyDown} event which is emitted by Stream Deck when an action is pressed. Stream Deck provides various events for tracking interaction
 	 * with devices including key down/up, dial rotations, and device connectivity, etc. When triggered, {@link ev} object contains information about the event including any payloads
 	 * and action information where applicable. In this example, our action will display a counter that increments by one each press. We track the current count on the action's persisted
 	 * settings using `setSettings` and `getSettings`.
 	 */
-	async onKeyDown(ev: streamDeck.KeyDownEvent<CounterSettings>): Promise<void> {
+	async onKeyDown(ev: KeyDownEvent<CounterSettings>): Promise<void> {
 		// Determine the current count from the settings.
 		let count = ev.payload.settings.count ?? 0;
 		count++;
--- template/src/plugin.ts
+++ template/src/plugin.ts
@@ -1,9 +1,15 @@
-import * as streamDeck from "@elgato/streamdeck";
+import streamDeck, { LogLevel } from "@elgato/streamdeck";
 
 import { IncrementCounter } from "./actions/increment-counter";
 
 // We can enable "trace" logging so that all messages between the Stream Deck, and the plugin are recorded. When storing sensitive information
-streamDeck.logger.setLevel(streamDeck.LogLevel.TRACE);
+streamDeck.logger.setLevel(LogLevel.TRACE);
 
 // Register the increment action.
-streamDeck.actions.registerAction("com.elgato.template.increment", new IncrementCounter());
+streamDeck.actions.registerAction({
+	uuid: "com.elgato.template.increment",
+	action: new IncrementCounter()
+});
+
+// Finally, connect to the Stream Deck.
+streamDeck.connect();
