5f3c6be0240aad4801689c46a7167e73b86e04f7
Richard Herman <geekyeggo@users.noreply.github.com>
2023-09-11 13:44:19 +0100

    feat: update restart and stop to display spinners

--- src/commands/restart.ts
+++ src/commands/restart.ts
@@ -1,6 +1,7 @@
 import chalk from "chalk";
 
 import { run } from "../common/runner";
+import { spin } from "../common/spinner";
 import { getStreamDeckPath, isPluginInstalled, isStreamDeckRunning } from "../stream-deck";
 
 /**
@@ -8,23 +9,25 @@ import { getStreamDeckPath, isPluginInstalled, isStreamDeckRunning } from "../st
  * @param uuid Unique-identifier that identifies the plugin.
  * @returns Promise resolved when the command has been executed.
  */
-export async function restart(uuid: string): Promise<void> {
-	// Check we have a plugin installed that matches the uuid.
-	if (!isPluginInstalled(uuid)) {
-		console.log(`No plugin found matching ${chalk.yellow(uuid)}`);
-		return;
-	}
+export function restart(uuid: string): Promise<void> {
+	return spin(`Restarting ${uuid}`, async ({ info, success, warn }) => {
+		// Check we have a plugin installed that matches the uuid.
+		if (!isPluginInstalled(uuid)) {
+			warn(`Plugin not found ${chalk.yellow(uuid)}`);
+			return;
+		}
 
-	const appPath = `"${getStreamDeckPath()}"`;
+		const appPath = `"${getStreamDeckPath()}"`;
 
-	// When Stream Deck isn't running, start it.
-	if (!(await isStreamDeckRunning())) {
-		await run(appPath, [], { detached: true, stderr: "ignore" });
-		console.log(chalk.yellow("Stream Deck is not running. Starting Stream Deck."));
-		return;
-	}
+		// When Stream Deck isn't running, start it.
+		if (!(await isStreamDeckRunning())) {
+			await run(appPath, [], { detached: true, stderr: "ignore" });
+			info("Stream Deck is not running. Starting Stream Deck.");
+			return;
+		}
 
-	// Restart the plugin.
-	await run(appPath, ["-r", uuid], { stderr: "ignore" });
-	console.log(chalk.green(`Successfully restarted ${uuid}`));
+		// Restart the plugin.
+		await run(appPath, ["-r", uuid], { stderr: "ignore" });
+		success(`Restarted ${chalk.green(uuid)} successfully`);
+	});
 }
--- src/commands/stop.ts
+++ src/commands/stop.ts
@@ -1,6 +1,7 @@
 import chalk from "chalk";
 
 import { run } from "../common/runner";
+import { spin } from "../common/spinner";
 import { getStreamDeckPath, isPluginInstalled, isStreamDeckRunning } from "../stream-deck";
 
 /**
@@ -8,20 +9,22 @@ import { getStreamDeckPath, isPluginInstalled, isStreamDeckRunning } from "../st
  * @param uuid Unique-identifier that identifies the plugin.
  * @returns Promise resolved when the command has been executed.
  */
-export async function stop(uuid: string): Promise<void> {
-	// Check we have a plugin installed that matches the uuid.
-	if (!isPluginInstalled(uuid)) {
-		console.log(`No plugin found matching ${chalk.yellow(uuid)}`);
-		return;
-	}
+export function stop(uuid: string): Promise<void> {
+	return spin(`Stopping ${uuid}`, async ({ info, success, warn }) => {
+		// Check we have a plugin installed that matches the uuid.
+		if (!isPluginInstalled(uuid)) {
+			warn(`Plugin not found ${chalk.yellow(uuid)}`);
+			return;
+		}
 
-	// When Stream Deck isn't running, warn the user.
-	if (!(await isStreamDeckRunning())) {
-		console.log(chalk.yellow("Stream Deck is not running."));
-		return;
-	}
+		// When Stream Deck isn't running, warn the user.
+		if (!(await isStreamDeckRunning())) {
+			info("Stream Deck is not running.");
+			return;
+		}
 
-	// Stop the plugin.
-	await run(`"${getStreamDeckPath()}"`, ["-s", uuid], { stderr: "ignore" });
-	console.log(chalk.green(`Successfully stopped ${uuid}`));
+		// Stop the plugin.
+		await run(`"${getStreamDeckPath()}"`, ["-s", uuid], { stderr: "ignore" });
+		success(`Stopped ${chalk.green(uuid)} successfully`);
+	});
 }
