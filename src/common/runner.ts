4322bf1d39dc28f605d45f898f2bfb2cca851730
Richard Herman <geekyeggo@users.noreply.github.com>
2023-09-11 15:59:18 +0100

    refactor: update runner to pipe error on exit code greater than 0

--- src/common/runner.ts
+++ src/common/runner.ts
@@ -1,4 +1,5 @@
-import child_process, { SpawnOptionsWithoutStdio, StdioNull } from "node:child_process";
+import child_process, { ChildProcessByStdio, SpawnOptionsWithStdioTuple, StdioNull, StdioPipe } from "node:child_process";
+import { Readable } from "node:stream";
 
 /**
  * Runs the specified {@link command} with the give {@link args}.
@@ -8,42 +9,56 @@ import child_process, { SpawnOptionsWithoutStdio, StdioNull } from "node:child_p
  * @returns The result of running the command. **NB.** when the command is detached, the result is always 0.
  */
 export function run(command: string, args: string[], options?: RunOptions): Promise<number> {
-	options = {
+	const opts: SpawnOptionsWithStdioTuple<StdioNull, StdioNull, StdioPipe> = {
 		...{
 			shell: true,
-			stdio: ["ignore", "ignore", options?.stderr || "inherit"],
 			windowsHide: true
 		},
-		...options
+		...options,
+		...{
+			stdio: ["ignore", "ignore", "pipe"]
+		}
 	};
 
 	return new Promise((resolve, reject) => {
-		const child = child_process.spawn(command, args, options);
+		const child = child_process.spawn(command, args, opts);
 
+		// When the process is detached, we can't monitor the result, so return success
 		if (options?.detached) {
 			child.unref();
 			resolve(0);
-		} else {
-			child
-				.on("message", (msg) => console.log(msg))
-				.on("error", (err) => console.log(err))
-				.on("exit", (code: number) => {
-					if (code > 0) {
-						reject(code);
-					} else {
-						resolve(0);
-					}
-				});
+			return;
 		}
+
+		// Begin gathering the stderr in the event we receive an error code.
+		const stderr = stderrReader(child);
+		child.on("exit", (code: number) => {
+			if (code > 0) {
+				stderr.then((value) => {
+					console.log(value);
+					reject(code);
+				});
+			} else {
+				resolve(0);
+			}
+		});
+	});
+}
+
+/**
+ * Reads the stderr to a buffer, and resolves the promise once it closes.
+ * @param process The process whose stderr should be monitored.
+ * @returns Buffered stderr as a string.
+ */
+async function stderrReader(process: ChildProcessByStdio<null, null, Readable>): Promise<string> {
+	return new Promise((resolve) => {
+		let stderr: ReadonlyArray<Uint8Array> = [];
+		process.stderr.on("data", (data) => (stderr = stderr.concat(data)));
+		process.stderr.once("close", () => resolve(Buffer.concat(stderr).toString()));
 	});
 }
 
 /**
  * Defines the options that support running a command via {@link run}.
  */
-export type RunOptions = Omit<SpawnOptionsWithoutStdio, "stdio"> & {
-	/**
-	 * Optional standard error stream; by default the standard error will be output to the default stream.
-	 */
-	stderr?: StdioNull;
-};
+export type RunOptions = Omit<SpawnOptionsWithStdioTuple<StdioNull, StdioNull, Readable>, "stdio">;
