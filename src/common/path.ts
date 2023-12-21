a821cee85c40ce93636f05dc2680da383ec0ec67
Richard Herman <1429781+GeekyEggo@users.noreply.github.com>
2023-12-21 15:48:29 +0000

    fix: add vscode prompt to create command (macOS) (#20)

--- src/common/path.ts
+++ src/common/path.ts
@@ -1,5 +1,40 @@
-import { dirname, resolve } from "node:path";
+import { existsSync, lstatSync, readdirSync, readlinkSync, Stats } from "node:fs";
+import { dirname, join, resolve } from "node:path";
 import { fileURLToPath } from "node:url";
+
+/**
+ * Determines whether the specified {@link application} is accessible from the environment's PATH variable and can be executed.
+ * @param application Name of the application to search for.
+ * @returns `true` when the application is considered executable; otherwise `false`.
+ */
+export function isExecutable(application: string): boolean {
+	for (const path of process.env.PATH?.split(":") || []) {
+		if (existsSync(path) && isDirectory(path) && readdirSync(path).find((entry) => entry === application && isFile(join(path, entry)))) {
+			return true;
+		}
+	}
+
+	return false;
+}
+
+/**
+ * Determines whether the specified {@link path}, or the path it links to, is a directory.
+ * @param path The path.
+ * @returns `true` when the path, or the path it links to, is a directory; otherwise `false`.
+ */
+export function isDirectory(path: string): boolean {
+	return checkStats(path, (stats) => stats?.isDirectory() === true);
+}
+
+/**
+ * Determines whether the specified {@link path}, or the path it links to, is a file.
+ * @param path The path.
+ * @returns `true` when the path, or the path it links to, is a file; otherwise `false`.
+ */
+export function isFile(path: string): boolean {
+	return checkStats(path, (stats) => stats?.isFile() === true);
+}
+
 /**
  * Validates the specified {@link value} represents a valid directory name.
  * @param value Value to validate.
@@ -27,6 +62,21 @@ export function isSafeBaseName(value: string): boolean {
  */
 export function relative(path: string): string {
 	return resolve(dirname(fileURLToPath(import.meta.url)), path);
+}
+
+/**
+ * Checks the stats of a given path and applies the {@link check} to them to determine the result.
+ * @param path Path to check; when the path represents a symbolic link, the link is referenced.
+ * @param check Function used to determine if the stats fulfil the check.
+ * @returns `true` when the stats of the {@link path} fulfil the {@link check}; otherwise `false`.
+ */
+function checkStats(path: string, check: (stats?: Stats) => boolean): boolean {
+	const stats = lstatSync(path);
+	if (stats?.isSymbolicLink()) {
+		return checkStats(readlinkSync(path), check);
+	}
+
+	return check(stats);
 }
 
 /**
