2f40dc5786c58dbedf19602659ef8a442eb2d15c
Richard Herman <geekyeggo@users.noreply.github.com>
2024-01-05 00:23:20 +0000

    perf: add manifest context to improve robustness of validation

---
+++ src/commands/validate/plugin/contexts/manifest.ts
@@ -0,0 +1,70 @@
+import { type Manifest } from "@elgato/streamdeck";
+import Ajv, { type AnySchema } from "ajv";
+import { type IOutputError } from "better-ajv-errors";
+import { existsSync, readFileSync } from "node:fs";
+import { join } from "node:path";
+import { validate, type JsonObject } from "../../../../common/json";
+import { relative } from "../../../../common/path";
+
+/**
+ * Provides information about the `manifest.json` file associated with a plugin.
+ */
+export class ManifestContext {
+	/**
+	 * Collection of JSON schema validation errors.
+	 */
+	public readonly errors: ReadonlyArray<IOutputError> = [];
+
+	/**
+	 * Parsed manifest data with all valid value types set, including the location of which the value was parsed within the JSON.
+	 */
+	public readonly manifest: JsonObject<Manifest> = {};
+
+	/**
+	 * Path to the `manifest.json` file; the file may or may not exist.
+	 */
+	public readonly path: string;
+
+	/**
+	 * JSON schema responsible for validating the `manifest.json` file.
+	 */
+	private readonly schema: AnySchema = ManifestContext.getSchema();
+
+	/**
+	 * Initializes a new instance of the {@link ManifestContext} class.
+	 * @param root Root directory where the plugin is located.
+	 */
+	constructor(root: string) {
+		// Determine if a manifest exists.
+		this.path = join(root, "manifest.json");
+		if (!existsSync(this.path)) {
+			return;
+		}
+
+		// Validate the parsed manifest against the JSON schema.
+		const json = readFileSync(this.path, { encoding: "utf-8" });
+		const ajv = new Ajv({ allErrors: true });
+		ajv.addKeyword("markdownDescription");
+
+		try {
+			({ errors: this.errors, value: this.manifest } = validate<Manifest>(json, ajv.compile<Manifest>(this.schema)));
+		} catch (e) {
+			if (e instanceof TypeError) {
+				this.errors = this.errors.concat([{ error: "Unable to parse JSON", start: { column: 0, line: 0, offset: 0 } }]);
+			}
+		}
+	}
+
+	/**
+	 * Gets the JSON schema used to validate the plugin's `manifest.json` file.
+	 * @returns The JSON schema.
+	 */
+	private static getSchema(): AnySchema {
+		try {
+			const path = relative("../node_modules/@elgato/streamdeck/schemas/manifest.json");
+			return JSON.parse(readFileSync(path, { encoding: "utf-8" }));
+		} catch (cause) {
+			throw new Error("Failed to parse manifest JSON schema", { cause });
+		}
+	}
+}
--- src/common/json.ts
+++ src/common/json.ts
@@ -1,4 +1,6 @@
-import type { DocumentNode, ElementNode, Location, ValueNode } from "@humanwhocodes/momoa";
+import { parse, type DocumentNode, type ElementNode, type Location, type MemberNode, type ObjectNode, type ValueNode } from "@humanwhocodes/momoa";
+import { type AnyValidateFunction } from "ajv/dist/types";
+import betterAjvErrors, { type IOutputError } from "better-ajv-errors";
 
 /**
  * Gets the location of the JSON {@link pointer} from the JSON's abstract syntax tree.
@@ -54,6 +56,142 @@ function traverse(node: Node, [pointer, ...pointers]: (number | string)[]): Node
 	}
 
 	throw new Error(`Unhandled node type ${node.type}`);
+}
+
+/**
+ * Validates the {@link json} against the {@link validate} schema function. The result is a data object from the parsed {@link json} string, where only valid value-types are set.
+ * Valid value types also include their location to enable better error logging.
+ * @param json JSON string to parse.
+ * @param validate Validate function responsible for validating the {@link json} against a schema.
+ * @returns Errors associated with validating the schema, and the parsed result of the {@link json}.
+ */
+export function validate<T extends object>(json: string, validate: AnyValidateFunction<T>): JsonValidationResult<T> {
+	// Check the JSON is valid, and we can parse it.
+	let data;
+	try {
+		data = JSON.parse(json);
+		validate(data);
+	} catch {
+		return {
+			errors: [{ error: "JSON contents are invalid", start: { column: 0, line: 0, offset: 0 } }],
+			value: {}
+		};
+	}
+
+	// Check the JSON represents an object (only objects are supported).
+	const ast = parse(json, { mode: "json", ranges: false, tokens: false });
+	if (ast.body.type !== "Object") {
+		return {
+			errors: [{ error: "JSON contents must represent a serialized object", start: { column: 0, line: 0, offset: 0 } }],
+			value: {}
+		};
+	}
+
+	// Prettify schema errors, and build the JSON as an object.
+	return {
+		errors: betterAjvErrors(validate.schema, data, validate.errors ?? [], { format: "js", json }),
+		value: toJsonObject(ast.body, validate.errors || [])
+	};
+}
+
+/**
+ * Result of validating a JSON string.
+ */
+type JsonValidationResult<T> = {
+	/**
+	 * Errors produced by validating the JSON against a schema.
+	 */
+	errors: IOutputError[];
+
+	/**
+	 * Parsed value, represented as a collection of values and their locations, mapped to the structure of {@template T}.
+	 */
+	value: JsonObject<T>;
+};
+
+/**
+ * JSON object that includes all valid value-types of {@template T}, and defines all invalid value-types as `undefined`.
+ */
+export type JsonObject<T = unknown> = {
+	[K in keyof T]?: JsonElement<T[K]>;
+};
+
+/**
+ * JSON element within a JSON string, including it's parsed value, and the location it was parsed from.
+ */
+export type JsonElement<T = unknown> = {
+	/**
+	 * Location of the element within the JSON it was parsed from.
+	 */
+	location?: Location | undefined;
+
+	/**
+	 * Parsed value.
+	 */
+	value?: T extends Array<infer E> ? JsonObject<E>[] : T extends object ? JsonObject<T> : T;
+};
+
+/**
+ * Maps the {@link node} to a {@link JsonObject}.
+ * @param node Source that contains the data.
+ * @param errors JSON schema errors; used to determine invalid types based on the instance path of an error.
+ * @returns The mapped JSON object.
+ */
+function toJsonObject<T>(node: ObjectNode, errors: AnyValidateFunction<T>["errors"]): JsonObject<T> {
+	const invalidTypeInstancePaths = new Set<string>(errors?.filter(({ keyword }) => keyword === "type").map(({ instancePath }) => instancePath) || []);
+
+	const reduce: JsonObjectReducer = (node, path) => {
+		if (invalidTypeInstancePaths.has(path)) {
+			return {
+				value: undefined,
+				location: node.loc?.start
+			};
+		}
+
+		// Object node, recursively reduce each member.
+		if (node.type === "Object") {
+			return node.members.reduce((obj: Record<string, unknown>, member: MemberNode) => {
+				obj[member.name.value] = reduce(member.value, `${path}/${member.name.value}`);
+				return obj;
+			}, {});
+		}
+
+		// Array node, recursively reduce each element.
+		if (node.type === "Array") {
+			return {
+				value: node.elements.map((item, i) => reduce(item.value, `${path}/${i}`)),
+				location: node.loc?.start
+			};
+		}
+
+		// Value node.
+		if (node.type === "Boolean" || node.type === "Number" || node.type === "String") {
+			return {
+				value: node.value,
+				location: node.loc?.start
+			};
+		}
+
+		// Null value node.
+		if (node.type === "Null") {
+			return {
+				value: null,
+				location: node.loc?.start
+			};
+		}
+
+		throw new Error(`Encountered unhandled node type '${node.type}' when mapping abstract-syntax tree node to JSON object`);
+	};
+
+	return reduce(node, "");
+}
+
+/**
+ * Reducer that traverses the {@link node} and maps it to a {@link JsonObject} or {@link JsonElement}.
+ */
+interface JsonObjectReducer {
+	(node: ObjectNode, path: string): JsonObject;
+	(node: Node, path: string): JsonElement;
 }
 
 /**
