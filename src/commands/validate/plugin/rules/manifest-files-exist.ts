1b462ed8740cbec49a42e200089934bdef04cb19
Richard Herman <geekyeggo@users.noreply.github.com>
2024-01-08 14:12:01 +0000

    feat: add file existence validation for all manifest files

---
+++ src/commands/validate/plugin/rules/manifest-files-exist.ts
@@ -0,0 +1,87 @@
+import { existsSync } from "node:fs";
+import { extname, join, resolve } from "node:path";
+import { type JsonElement } from "../../../../common/json";
+import { ImagePathResolution, imagePathResolution, resolveImagePath } from "../../../../stream-deck";
+import { rule } from "../../rule";
+import { type PluginContext } from "../contexts/plugin";
+
+/**
+ * Validates the files defined within the manifest exist.
+ */
+export const manifestFilesExist = rule<PluginContext>(function (plugin: PluginContext) {
+	/**
+	 * Validates the specified {@link elem} file exists.
+	 * @param elem JSON element that contains information about the file to validate.
+	 * @param ext File extension.
+	 */
+	const fileExists = (elem: JsonElement<string> | undefined, ext: string = ""): void => {
+		if (elem?.value === undefined) {
+			return;
+		}
+
+		const path = resolve(this.path, elem.value, ext);
+		if (!existsSync(path)) {
+			this.addError(plugin.manifest.path, `${elem.pointer}: '${elem.value}' file not found`, {
+				position: elem.location,
+				suggestion: ext !== "" ? `File must be ${ext}` : undefined
+			});
+		}
+	};
+
+	/**
+	 * Validates the specified {@link elem} image file exists.
+	 * @param elem JSON element that contains information about the image to validate.
+	 * @param type Image path resolution type.
+	 */
+	const imageExists = (elem: JsonElement<string> | undefined, type: ImagePathResolution = imagePathResolution.default): void => {
+		if (elem?.value === undefined) {
+			return;
+		}
+
+		const path = resolveImagePath(this.path, elem.value, type);
+		if (path === undefined) {
+			this.addError(plugin.manifest.path, `${elem.pointer}: '${elem.value}' file not found`, {
+				suggestion: `Image must be ${type.join(", ")}`,
+				position: elem.location
+			});
+		} else if (extname(path) === ".png" && !existsSync(join(this.path, `${elem.value}@2x.png`))) {
+			this.addWarning(path, "Missing high-resolution (@2x) variant");
+		}
+	};
+
+	// Top-level.
+	fileExists(plugin.manifest.manifest.CodePath);
+	fileExists(plugin.manifest.manifest.CodePathMac);
+	fileExists(plugin.manifest.manifest.CodePathWin);
+	imageExists(plugin.manifest.manifest.Icon);
+	imageExists(plugin.manifest.manifest.CategoryIcon);
+	fileExists(plugin.manifest.manifest.PropertyInspectorPath);
+
+	// Action files.
+	plugin.manifest.manifest.Actions?.forEach((action) => {
+		imageExists(action?.Icon);
+		imageExists(action?.Encoder?.background, imagePathResolution.encoderBackground);
+		imageExists(action?.Encoder?.Icon);
+		fileExists(action.PropertyInspectorPath);
+		if (!isPredefinedLayout(action.Encoder?.layout)) {
+			fileExists(action.Encoder?.layout);
+		}
+
+		action.States?.forEach((state) => {
+			imageExists(state?.Image);
+			imageExists(state?.MultiActionImage);
+		});
+	});
+
+	// Profile files.
+	plugin.manifest.manifest.Profiles?.forEach((profile) => fileExists(profile.Name, ".streamDeckProfile"));
+});
+
+/**
+ * Determines whether the {@link elem} represents a pre-defined layout; this is guarded by the manifest's JSON schema validation.
+ * @param elem JSON element that contains the information about the layout.
+ * @returns `true` when the value represents a pre-defined layout.
+ */
+function isPredefinedLayout(elem: JsonElement<string> | undefined): boolean {
+	return elem?.value?.startsWith("$") === true && !elem.value.endsWith(".json");
+}
--- src/commands/validate/plugin/validate.ts
+++ src/commands/validate/plugin/validate.ts
@@ -1,9 +1,9 @@
 import type { ValidationResult } from "../result";
 import { validate } from "../validator";
 import { PluginContext } from "./contexts/plugin";
-import { actionImagesExist } from "./rules/action-images-exist";
 import { directoryNameIsIdentifier } from "./rules/directory-name-is-uuid";
 import { manifestExistsAndSchemaIsValid } from "./rules/manifest-exists-and-schema-valid";
+import { manifestFilesExist } from "./rules/manifest-files-exist";
 import { pathIsDirectory } from "./rules/path-is-directory";
 
 /**
@@ -12,5 +12,5 @@ import { pathIsDirectory } from "./rules/path-is-directory";
  * @returns The validation result.
  */
 export function validatePlugin(path: string): ValidationResult {
-	return validate<PluginContext>(path, new PluginContext(path), [pathIsDirectory, directoryNameIsIdentifier, manifestExistsAndSchemaIsValid, actionImagesExist]);
+	return validate<PluginContext>(path, new PluginContext(path), [pathIsDirectory, directoryNameIsIdentifier, manifestExistsAndSchemaIsValid, manifestFilesExist]);
 }
