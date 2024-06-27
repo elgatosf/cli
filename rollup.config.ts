3ce1bf51d30d7fddb89124e976889df1541aa97e
Richard Herman <geekyeggo@users.noreply.github.com>
2024-06-27 16:57:07 +0100

    build: update exports to not include dependencies

--- rollup.config.ts
+++ rollup.config.ts
@@ -9,19 +9,29 @@ import { RollupOptions } from "rollup";
 
 const isWatching = !!process.env.ROLLUP_WATCH;
 
-/**
- * Ignore @elgato/schema to enable auto-update.
- */
+const banner = `#!/usr/bin/env node
+
+/**!
+ * @author Elgato
+ * @module elgato/streamdeck
+ * @license MIT
+ * @copyright Copyright (c) Corsair Memory Inc.
+ */`
+
+// Ignore @elgato/schema to enable auto-update.
 const external = [
 	"@elgato/schemas",
 	"@elgato/schemas/streamdeck/plugins/",
 	"@elgato/schemas/streamdeck/plugins/json",
 ];
 
+/**
+ * CLI bundling.
+ */
 const options: RollupOptions = {
     input: "src/cli.ts",
     output: {
-        banner: "#!/usr/bin/env node",
+        banner,
         file: "bin/streamdeck.mjs",
         sourcemap: isWatching,
         sourcemapPathTransform: (relativeSourcePath: string, sourcemapPath: string): string => {
